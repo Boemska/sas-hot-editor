ec51c87475bdab4ce563f480a7d98bfc309e692f
Bojan Djurdjevic <bojan88@gmail.com>
2015-12-23 12:58:00 +0100

    Multiple updates on debug window fixed

--- app/debugWindow/debugWindow.js
+++ app/debugWindow/debugWindow.js
@@ -2,30 +2,26 @@ angular.module('h54sDebugWindow', ['sasAdapter'])
 
 .controller('debugWindowCtrl', ['$scope', 'sasAdapter', '$rootScope', '$sce', function($scope, sasAdapter, $rootScope, $sce) {
 
-  $rootScope.$watch('showDebugWindow', function() {
-    if($rootScope.showDebugWindow) {
-      $scope.appLogs = sasAdapter.getApplicationLogs();
-      $scope.debugData = sasAdapter.getDebugData().map(function(el) {
-        return {
-          time: el.time,
-          message: $sce.trustAsHtml(el.debugHtml.replace(/<style.+>(.|\n)+<\/style>/g, '').replace(/<link.+>/g, '')),
-          sasProgram: el.sasProgram
-        };
-      });
-      $scope.sasErrors = sasAdapter.getSasErrors();
-      $scope.failedRequests = sasAdapter.getFailedRequests().map(function(el) {
-        return {
-          time: el.time,
-          message: $sce.trustAsHtml(el.responseHtml.replace(/<style.+>(.|\n)+<\/style>/g, '').replace(/<link.+>/g, '')),
-          sasProgram: el.sasProgram
-        };
-      });
-
-      var headerHeight = $('#debugWindow .nav.nav-tabs').height();
-      var height = $('#debugWindow').height() - headerHeight - 10;
-      $('#debugWindow .tab-content').height(height);
-    }
+  $scope.appLogs = sasAdapter.getApplicationLogs();
+  $scope.debugData = sasAdapter.getDebugData().map(function(el) {
+    return {
+      time: el.time,
+      message: $sce.trustAsHtml(el.debugHtml.replace(/<style.+>(.|\n)+<\/style>/g, '').replace(/<link.+>/g, '')),
+      sasProgram: el.sasProgram
+    };
   });
+  $scope.sasErrors = sasAdapter.getSasErrors();
+  $scope.failedRequests = sasAdapter.getFailedRequests().map(function(el) {
+    return {
+      time: el.time,
+      message: $sce.trustAsHtml(el.responseHtml.replace(/<style.+>(.|\n)+<\/style>/g, '').replace(/<link.+>/g, '')),
+      sasProgram: el.sasProgram
+    };
+  });
+
+  var headerHeight = $('#debugWindow .nav.nav-tabs').height();
+  var height = $('#debugWindow').height() - headerHeight - 10;
+  $('#debugWindow .tab-content').height(height);
 
   $scope.closeDebugWindow = function() {
     $rootScope.showDebugWindow = false;
@@ -33,22 +29,26 @@ angular.module('h54sDebugWindow', ['sasAdapter'])
 
   $scope.clearSasErrors = function() {
     sasAdapter.clearSasErrors();
-    $scope.sasErrors = [];
+    //TODO: remove when h54s is updated to a version greater than 0.7.3
+    $scope.sasErrors.length = 0;
   };
 
   $scope.clearDebugData = function() {
     sasAdapter.clearDebugData();
-    $scope.debugData = [];
+    //TODO: remove when h54s is updated to a version greater than 0.7.3
+    $scope.debugData.length = 0;
   };
 
   $scope.clearApplicationLogs = function() {
     sasAdapter.clearApplicationLogs();
-    $scope.appLogs = [];
+    //TODO: remove when h54s is updated to a version greater than 0.7.3
+    $scope.appLogs.length = 0;
   };
 
   $scope.clearFailedRequests = function() {
     sasAdapter.clearFailedRequests();
-    $scope.failedRequests = [];
+    //TODO: remove when h54s is updated to a version greater than 0.7.3
+    $scope.failedRequests.length = 0;
   };
 
 }]);
