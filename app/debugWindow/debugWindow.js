0a286db014972b05d9afb9a6eea6ad562015231d
Bojan Djurdjevic <bojan@boemskats.com>
2016-03-09 12:34:13 +0100

    Better binding for debug data and failed requests

--- app/debugWindow/debugWindow.js
+++ app/debugWindow/debugWindow.js
@@ -3,21 +3,31 @@ angular.module('h54sDebugWindow', ['sasAdapter'])
 .controller('debugWindowCtrl', ['$scope', 'sasAdapter', '$rootScope', '$sce', function($scope, sasAdapter, $rootScope, $sce) {
 
   $scope.appLogs = sasAdapter.getApplicationLogs();
-  $scope.debugData = sasAdapter.getDebugData().map(function(el) {
-    return {
-      time: el.time,
-      message: $sce.trustAsHtml(el.debugHtml.replace(/<style.+>(.|\n)+<\/style>/g, '').replace(/<link.+>/g, '')),
-      sasProgram: el.sasProgram
-    };
-  });
   $scope.sasErrors = sasAdapter.getSasErrors();
-  $scope.failedRequests = sasAdapter.getFailedRequests().map(function(el) {
-    return {
-      time: el.time,
-      message: $sce.trustAsHtml(el.responseHtml.replace(/<style.+>(.|\n)+<\/style>/g, '').replace(/<link.+>/g, '')),
-      sasProgram: el.sasProgram
-    };
-  });
+
+  $scope.$watch(function() {
+    return sasAdapter.getDebugData();
+  }, function(debugData) {
+    $scope.debugData = debugData.map(function(el) {
+      return {
+        time: el.time,
+        message: $sce.trustAsHtml(el.debugHtml.replace(/<style.+>(.|\n)+<\/style>/g, '').replace(/<link.+>/g, '')),
+        sasProgram: el.sasProgram
+      };
+    });
+  }, true);
+
+  $scope.$watch(function() {
+    return sasAdapter.getFailedRequests();
+  }, function(failedRequests) {
+    $scope.failedRequests = failedRequests.map(function(el) {
+      return {
+        time: el.time,
+        message: $sce.trustAsHtml(el.responseHtml.replace(/<style.+>(.|\n)+<\/style>/g, '').replace(/<link.+>/g, '')),
+        sasProgram: el.sasProgram
+      };
+    });
+  }, true);
 
   var headerHeight = $('#debugWindow .nav.nav-tabs').height();
   var height = $('#debugWindow').height() - headerHeight - 10;
@@ -33,7 +43,6 @@ angular.module('h54sDebugWindow', ['sasAdapter'])
 
   $scope.clearDebugData = function() {
     sasAdapter.clearDebugData();
-    $scope.debugData.length = 0;
   };
 
   $scope.clearApplicationLogs = function() {
@@ -42,7 +51,6 @@ angular.module('h54sDebugWindow', ['sasAdapter'])
 
   $scope.clearFailedRequests = function() {
     sasAdapter.clearFailedRequests();
-    $scope.failedRequests.length = 0;
   };
 
 }]);
