c67bf25c4bf2376834b3d01aea1577066e44495a
bojan88 <bojan@boemskats.com>
2016-05-25 10:30:04 +0200

    Added select inputs for library and table and loading table on button click

--- app/main/mainPage.js
+++ app/main/mainPage.js
@@ -12,8 +12,41 @@ angular.module('myApp.main', ['ngRoute', 'dynamicHandsontable'])
 
 .controller('SideCtrl', [
   '$scope',
-  function($scope) {
+  'sasAdapter',
+  function($scope, sasAdapter) {
+    var tablesMap = {};
+    $scope.libs = [];
+    $scope.tables = [];
 
+    $scope.$watch('sideData.library', function(newValue) {
+      //moved to next event loop tick with timeout
+      //setting timeout enables the selected item to finish animation first and then update the Tables select
+      setTimeout(function() {
+        $scope.tables = tablesMap[newValue];
+      }, 0);
+
+      //deselect table if it doesn't exist in sub array
+      if(tablesMap[newValue] && tablesMap[newValue].indexOf($scope.sideData.table) === -1) {
+        $scope.sideData.table = undefined;
+      }
+    });
+
+    sasAdapter.call('/Apps/tableEditor/startupService').then(function(res) {
+      res.libsmems.forEach(function(lib) {
+        if($scope.libs.indexOf(lib.LIBNAME) === -1) {
+          $scope.libs.push(lib.LIBNAME);
+        }
+        //create sub array if it doesn't exist
+        if(!tablesMap[lib.LIBNAME]) {
+          tablesMap[lib.LIBNAME] = [];
+        }
+        if(tablesMap[lib.LIBNAME].indexOf(lib.MEMNAME) === -1) {
+          tablesMap[lib.LIBNAME].push(lib.MEMNAME);
+        }
+      });
+    }, function(err) {
+      alert(err);
+    });
   }
 ])
 
@@ -23,6 +56,8 @@ angular.module('myApp.main', ['ngRoute', 'dynamicHandsontable'])
   '$rootScope',
   '$mdToast',
   function ($scope, sasAdapter, $rootScope, $mdToast) {
+    $scope.sideData = {}; //used in child scope of SideCtrl
+
     var toast = $mdToast.build({
       hideDelay: 1800,
       position: 'bottom right'
@@ -33,16 +68,19 @@ angular.module('myApp.main', ['ngRoute', 'dynamicHandsontable'])
       $mdToast.show(toast);
     };
 
-    var table = sasAdapter.createTable([
-      {libname: "TESTDATA", memname: "CLASS"}
-    ], 'data');
 
-    sasAdapter.call('/Apps/tableEditor/getTable', table).then(function(res) {
-      $scope.htDynamicSpec = res.columnspec;
-      $scope.htData = res.tabledata;
-    }, function(err) {
-      alert(err);
-    });
+    $scope.open = function() {
+      var table = sasAdapter.createTable([
+        {libname: $scope.sideData.library, memname: $scope.sideData.table}
+      ], 'data');
+
+      sasAdapter.call('/Apps/tableEditor/getTable', table).then(function(res) {
+        $scope.htDynamicSpec = res.columnspec;
+        $scope.htData = res.tabledata;
+      }, function(err) {
+        alert(err);
+      });
+    };
 
     $scope.save = function() {
       table.add($scope.htData, 'tabledata');
