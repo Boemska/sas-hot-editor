29eebad5e2ff3347929de6c99ccd6f1808b538de
bojan88 <bojan@boemskats.com>
2016-06-24 17:11:35 +0200

    Dependency injection fix

--- app/components/sasAdapter.js
+++ app/components/sasAdapter.js
@@ -1,140 +1,148 @@
 angular.module('sasAdapter', ['ngToast', 'ngAnimate', 'ngSanitize'])
 
-.factory('sasAdapter', function($q, $rootScope, ngToast, $timeout, $mdDialog, $mdMedia, $mdToast) {
-  var _adapter = new h54s({
-    isRemoteConfig: true,
-    debug: localStorage.getItem('h54sDebug') && localStorage.getItem('h54sDebug') === 'true'
-  });
-  return {
-    login: function(user, pass) {
-      var deferred = $q.defer();
-      try {
-        _adapter.login(user, pass, function(status) {
-          deferred.resolve(status);
-        });
-      } catch(e) {
-        deferred.reject(e);
-      }
-      return deferred.promise;
-    },
-    call: function(sasProgram, table) {
-      var deferred = $q.defer();
-      var loadingToastId = ngToast.create({
-        className: 'info',
-        content: 'Loading: <b>' + sasProgram + '</b>'
-      });
-
-      _adapter.call(sasProgram, table, function(err, res) {
-        var toast;
-        for(var i = 0; i < ngToast.messages.length; i++) {
-          if(loadingToastId === ngToast.messages[i].id) {
-            toast = ngToast.messages[i];
-            break;
-          }
-        }
-
-        if(err && (err.type === 'notLoggedinError' || err.type === 'loginError')) {
-          ngToast.dismiss();
-          var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
-          $mdDialog.show({
-            controller: 'LoginModalCtrl',
-            templateUrl: 'loginModal/loginModal.html',
-            parent: angular.element(document.body),
-            fullscreen: useFullScreen,
-            escapeToClose: false
+.factory('sasAdapter', [
+  '$q',
+  '$rootScope',
+  'ngToast',
+  '$timeout',
+  '$mdDialog',
+  '$mdMedia',
+  '$mdToast',
+  function($q, $rootScope, ngToast, $timeout, $mdDialog, $mdMedia, $mdToast) {
+    var _adapter = new h54s({
+      isRemoteConfig: true,
+      debug: localStorage.getItem('h54sDebug') && localStorage.getItem('h54sDebug') === 'true'
+    });
+    return {
+      login: function(user, pass) {
+        var deferred = $q.defer();
+        try {
+          _adapter.login(user, pass, function(status) {
+            deferred.resolve(status);
           });
-          return;
+        } catch(e) {
+          deferred.reject(e);
         }
+        return deferred.promise;
+      },
+      call: function(sasProgram, table) {
+        var deferred = $q.defer();
+        var loadingToastId = ngToast.create({
+          className: 'info',
+          content: 'Loading: <b>' + sasProgram + '</b>'
+        });
 
-        if(err) {
-          if(toast) {
-            toast.className = 'danger';
-            toast.content = 'Error loading <b>' + sasProgram + '</b>';
-          } else {
-            ngToast.create({
-              className: 'danger',
-              content: 'Error loading <b>' + sasProgram + '</b>'
-            });
+        _adapter.call(sasProgram, table, function(err, res) {
+          var toast;
+          for(var i = 0; i < ngToast.messages.length; i++) {
+            if(loadingToastId === ngToast.messages[i].id) {
+              toast = ngToast.messages[i];
+              break;
+            }
           }
-          deferred.reject(err);
+
+          if(err && (err.type === 'notLoggedinError' || err.type === 'loginError')) {
+            ngToast.dismiss();
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
+            $mdDialog.show({
+              controller: 'LoginModalCtrl',
+              templateUrl: 'loginModal/loginModal.html',
+              parent: angular.element(document.body),
+              fullscreen: useFullScreen,
+              escapeToClose: false
+            });
+            return;
+          }
+
+          if(err) {
+            if(toast) {
+              toast.className = 'danger';
+              toast.content = 'Error loading <b>' + sasProgram + '</b>';
+            } else {
+              ngToast.create({
+                className: 'danger',
+                content: 'Error loading <b>' + sasProgram + '</b>'
+              });
+            }
+            deferred.reject(err);
+          } else {
+            if(toast) {
+              toast.className = 'success';
+              toast.content = 'Loaded: <b>' + sasProgram + '</b>';
+            } else {
+              ngToast.create({
+                className: 'success',
+                content: 'Loaded: <b>' + sasProgram + '</b>'
+              });
+            }
+
+            if(res.usermessage !== 'blank') {
+              $mdToast.show(
+                $mdToast.simple()
+                  .textContent(res.usermessage)
+                  .position('bottom right')
+                  .hideDelay(10000)
+                  .action('OK')
+              );
+            }
+
+            deferred.resolve(res);
+          }
+
+          $timeout(function() {
+            if(toast) {
+              ngToast.dismiss(toast.id);
+            }
+          }, 1500);
+        });
+        return deferred.promise;
+      },
+      createTable: function(table, macro) {
+        return new h54s.Tables(table, macro);
+      },
+      toggleDebugMode: function() {
+        if(!_adapter.debug) {
+          _adapter.setDebugMode();
+          $rootScope.debugMode = true;
+          localStorage.setItem('h54sDebug', true);
         } else {
-          if(toast) {
-            toast.className = 'success';
-            toast.content = 'Loaded: <b>' + sasProgram + '</b>';
-          } else {
-            ngToast.create({
-              className: 'success',
-              content: 'Loaded: <b>' + sasProgram + '</b>'
-            });
-          }
-
-          if(res.usermessage !== 'blank') {
-            $mdToast.show(
-              $mdToast.simple()
-                .textContent(res.usermessage)
-                .position('bottom right')
-                .hideDelay(10000)
-                .action('OK')
-            );
-          }
-
-          deferred.resolve(res);
+          _adapter.unsetDebugMode();
+          $rootScope.debugMode = false;
+          localStorage.setItem('h54sDebug', false);
         }
-
-        $timeout(function() {
-          if(toast) {
-            ngToast.dismiss(toast.id);
-          }
-        }, 1500);
-      });
-      return deferred.promise;
-    },
-    createTable: function(table, macro) {
-      return new h54s.Tables(table, macro);
-    },
-    toggleDebugMode: function() {
-      if(!_adapter.debug) {
-        _adapter.setDebugMode();
-        $rootScope.debugMode = true;
-        localStorage.setItem('h54sDebug', true);
-      } else {
-        _adapter.unsetDebugMode();
-        $rootScope.debugMode = false;
-        localStorage.setItem('h54sDebug', false);
+      },
+      getDebugData: function() {
+        return _adapter.getDebugData();
+      },
+      getApplicationLogs: function() {
+        return _adapter.getApplicationLogs();
+      },
+      getSasErrors: function() {
+        return _adapter.getSasErrors();
+      },
+      getFailedRequests: function() {
+        return _adapter.getFailedRequests();
+      },
+      setCredentials: function(user, pass) {
+        return _adapter.setCredentials(user, pass);
+      },
+      clearApplicationLogs: function() {
+        _adapter.clearApplicationLogs();
+      },
+      clearDebugData: function() {
+        _adapter.clearDebugData();
+      },
+      clearSasErrors: function() {
+        _adapter.clearSasErrors();
+      },
+      clearFailedRequests: function() {
+        _adapter.clearFailedRequests();
+      },
+      isDebugMode : function() {
+        return _adapter.debug;
+      },
+      onRemoteConfigUpdate: function(callback) {
+        _adapter.onRemoteConfigUpdate(callback);
       }
-    },
-    getDebugData: function() {
-      return _adapter.getDebugData();
-    },
-    getApplicationLogs: function() {
-      return _adapter.getApplicationLogs();
-    },
-    getSasErrors: function() {
-      return _adapter.getSasErrors();
-    },
-    getFailedRequests: function() {
-      return _adapter.getFailedRequests();
-    },
-    setCredentials: function(user, pass) {
-      return _adapter.setCredentials(user, pass);
-    },
-    clearApplicationLogs: function() {
-      _adapter.clearApplicationLogs();
-    },
-    clearDebugData: function() {
-      _adapter.clearDebugData();
-    },
-    clearSasErrors: function() {
-      _adapter.clearSasErrors();
-    },
-    clearFailedRequests: function() {
-      _adapter.clearFailedRequests();
-    },
-    isDebugMode : function() {
-      return _adapter.debug;
-    },
-    onRemoteConfigUpdate: function(callback) {
-      _adapter.onRemoteConfigUpdate(callback);
-    }
-  };
-});
+    };
+}]);
--- app/dynamicHandsontable/directive.js
+++ app/dynamicHandsontable/directive.js
@@ -17,154 +17,158 @@ angular.module('dynamicHandsontable', ['ngHandsontable'])
         height: '@'
       },
       templateUrl: 'dynamicHandsontable/template.html',
-      controller: function($scope, $element) {
-        $scope.hotId = 'table-' + Math.random().toString(16).slice(2);
+      controller: [
+        '$scope',
+        '$element',
+        function($scope, $element) {
+          $scope.hotId = 'table-' + Math.random().toString(16).slice(2);
 
-        var getType = function(typeInt) {
-          switch(typeInt) {
-            case 2:
-              return 'text';
-            case 1:
-              return 'numeric';
-          }
-        };
-
-        angular.element($window).bind('resize', function() {
-          var tableWidth = 0;
-          if(!$scope.spec) {
-            return;
-          }
-
-          setTimeout(function() {
-            if($scope.width) {
-              tableWidth = $scope.width;
-            } else {
-              var maxWidth = $element.parent().parent()[0].clientWidth - 32; //32px for margin
-              $scope.spec.forEach(function(s) {
-                tableWidth += parseInt(s.LENGTH) * 15;
-              });
-              tableWidth = Math.min(tableWidth, maxWidth);
+          var getType = function(typeInt) {
+            switch(typeInt) {
+              case 2:
+                return 'text';
+              case 1:
+                return 'numeric';
             }
-            updateTableSettings({width: tableWidth});
-          }, 500);
-        });
+          };
 
-        $scope.$watch('spec', function() {
-          if($scope.spec && $scope.data) {
+          angular.element($window).bind('resize', function() {
             var tableWidth = 0;
-
-            $scope.columns = $scope.spec.map(function(s) {
-              return {
-                // type: getType(s.TYPE),
-                data: s.NAME.toUpperCase(),
-                title: s.NAME
-              };
-            });
-
-            if(!$scope.width) {
-              var maxWidth = $element.parent().parent()[0].clientWidth - 32; //32px for margin
-              $scope.spec.forEach(function(s) {
-                tableWidth += parseInt(s.LENGTH) * 15;
-              });
-              tableWidth = Math.min(tableWidth, maxWidth);
+            if(!$scope.spec) {
+              return;
             }
 
-            updateTableSettings({width: $scope.width || tableWidth});
+            setTimeout(function() {
+              if($scope.width) {
+                tableWidth = $scope.width;
+              } else {
+                var maxWidth = $element.parent().parent()[0].clientWidth - 32; //32px for margin
+                $scope.spec.forEach(function(s) {
+                  tableWidth += parseInt(s.LENGTH) * 15;
+                });
+                tableWidth = Math.min(tableWidth, maxWidth);
+              }
+              updateTableSettings({width: tableWidth});
+            }, 500);
+          });
 
-            $scope.settings = {
-              autoWrapRow: true,
-              stretchH: 'all',
-              width: $scope.width || tableWidth,
-              beforeChange: function (changes) {
-                if(changes.length === 0) return;
+          $scope.$watch('spec', function() {
+            if($scope.spec && $scope.data) {
+              var tableWidth = 0;
 
-                var instance = hotRegisterer.getInstance($scope.hotId);
+              $scope.columns = $scope.spec.map(function(s) {
+                return {
+                  // type: getType(s.TYPE),
+                  data: s.NAME.toUpperCase(),
+                  title: s.NAME
+                };
+              });
 
-                for(var i = 0; i < $scope.spec.length; i++) {
-                  for(var j = 0; j < changes.length; j++) {
-                    if(changes[j][1] === $scope.spec[i].NAME.toUpperCase()) {
-                      var colIndex = $scope.columns.map(function(o) {
-                        return o.data;
-                      }).indexOf(changes[j][1]);
+              if(!$scope.width) {
+                var maxWidth = $element.parent().parent()[0].clientWidth - 32; //32px for margin
+                $scope.spec.forEach(function(s) {
+                  tableWidth += parseInt(s.LENGTH) * 15;
+                });
+                tableWidth = Math.min(tableWidth, maxWidth);
+              }
 
-                      if(getType($scope.spec[i].TYPE) !== 'numeric') {
-                        if(changes[j][3] && changes[j][3].length > $scope.spec[i].LENGTH) {
-                          if($scope.spec[i].LENGTH.toString().slice(-1) === '1') {
-                            $scope.errorHandler('Max length of ' + $scope.spec[i].LENGTH + ' character exceeded');
+              updateTableSettings({width: $scope.width || tableWidth});
+
+              $scope.settings = {
+                autoWrapRow: true,
+                stretchH: 'all',
+                width: $scope.width || tableWidth,
+                beforeChange: function (changes) {
+                  if(changes.length === 0) return;
+
+                  var instance = hotRegisterer.getInstance($scope.hotId);
+
+                  for(var i = 0; i < $scope.spec.length; i++) {
+                    for(var j = 0; j < changes.length; j++) {
+                      if(changes[j][1] === $scope.spec[i].NAME.toUpperCase()) {
+                        var colIndex = $scope.columns.map(function(o) {
+                          return o.data;
+                        }).indexOf(changes[j][1]);
+
+                        if(getType($scope.spec[i].TYPE) !== 'numeric') {
+                          if(changes[j][3] && changes[j][3].length > $scope.spec[i].LENGTH) {
+                            if($scope.spec[i].LENGTH.toString().slice(-1) === '1') {
+                              $scope.errorHandler('Max length of ' + $scope.spec[i].LENGTH + ' character exceeded');
+                            } else {
+                              $scope.errorHandler('Max length of ' + $scope.spec[i].LENGTH + ' characters exceeded');
+                            }
+                            instance.setCellMeta(changes[j][0], colIndex, 'valid', false);
                           } else {
-                            $scope.errorHandler('Max length of ' + $scope.spec[i].LENGTH + ' characters exceeded');
+                            instance.setCellMeta(changes[j][0], colIndex, 'valid', true);
                           }
-                          instance.setCellMeta(changes[j][0], colIndex, 'valid', false);
                         } else {
-                          instance.setCellMeta(changes[j][0], colIndex, 'valid', true);
-                        }
-                      } else {
-                        if(isNaN(changes[j][3])) {
-                          $scope.errorHandler('Only numeric values are accepted');
-                          instance.setCellMeta(changes[j][0], colIndex, 'valid', false);
-                        } else {
-                          instance.setCellMeta(changes[j][0], colIndex, 'valid', true);
-                          changes[j][3] = changes[j][3] && parseFloat(changes[j][3]);
-                        }
+                          if(isNaN(changes[j][3])) {
+                            $scope.errorHandler('Only numeric values are accepted');
+                            instance.setCellMeta(changes[j][0], colIndex, 'valid', false);
+                          } else {
+                            instance.setCellMeta(changes[j][0], colIndex, 'valid', true);
+                            changes[j][3] = changes[j][3] && parseFloat(changes[j][3]);
+                          }
 
-                        if(changes[j][3] === '') {
-                          changes[j][3] = null;
+                          if(changes[j][3] === '') {
+                            changes[j][3] = null;
+                          }
                         }
                       }
                     }
                   }
-                }
-              },
-              afterChange: function(changes, source) {
-                if(source === 'loadData') {
-                  $timeout(function() {
-                    insertEmptyRow();
-                  }, 0);
-                  return;
-                }
-
-                var instance = hotRegisterer.getInstance($scope.hotId);
-
-                var rowInd;
-                for(var i = changes.length - 1; i >= 0; i--) {
-                  //skip this change if we had another from the same row
-                  if(rowInd === changes[i][0]) {
-                    continue;
+                },
+                afterChange: function(changes, source) {
+                  if(source === 'loadData') {
+                    $timeout(function() {
+                      insertEmptyRow();
+                    }, 0);
+                    return;
                   }
-                  rowInd = changes[i][0];
-                  //if it's last row and it's empty
-                  if(rowInd === instance.countRows() - 1 && !instance.isEmptyRow(instance.countRows() - 1)) {
-                    insertEmptyRow();
-                    break;
-                  } else if(rowInd !== instance.countRows() - 1 && instance.isEmptyRow(changes[i][0])) {
-                    instance.alter('remove_row', rowInd);
+
+                  var instance = hotRegisterer.getInstance($scope.hotId);
+
+                  var rowInd;
+                  for(var i = changes.length - 1; i >= 0; i--) {
+                    //skip this change if we had another from the same row
+                    if(rowInd === changes[i][0]) {
+                      continue;
+                    }
+                    rowInd = changes[i][0];
+                    //if it's last row and it's empty
+                    if(rowInd === instance.countRows() - 1 && !instance.isEmptyRow(instance.countRows() - 1)) {
+                      insertEmptyRow();
+                      break;
+                    } else if(rowInd !== instance.countRows() - 1 && instance.isEmptyRow(changes[i][0])) {
+                      instance.alter('remove_row', rowInd);
+                    }
                   }
+                  rowInd = null;
+
+                  $scope.onDataChange(changes, instance);
                 }
-                rowInd = null;
+              };
+            } else {
+              $scope.columns = null;
+            }
+          });
 
-                $scope.onDataChange(changes, instance);
-              }
-            };
-          } else {
-            $scope.columns = null;
+          function insertEmptyRow() {
+            var instance = hotRegisterer.getInstance($scope.hotId);
+            instance.alter('insert_row');
           }
-        });
 
-        function insertEmptyRow() {
-          var instance = hotRegisterer.getInstance($scope.hotId);
-          instance.alter('insert_row');
-        }
-
-        function updateTableSettings(settings) {
-          var instance = hotRegisterer.getInstance($scope.hotId);
-          if(instance) {
-            //the is already present, but width needs to be updated
-            setTimeout(function() {
-              instance.updateSettings(settings);
-            }, 0);
+          function updateTableSettings(settings) {
+            var instance = hotRegisterer.getInstance($scope.hotId);
+            if(instance) {
+              //the is already present, but width needs to be updated
+              setTimeout(function() {
+                instance.updateSettings(settings);
+              }, 0);
+            }
           }
         }
-      }
+      ]
     };
   }
 ]);
